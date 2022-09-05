c9b54c676a1a35680272470d4f682055993cd2c1
Janarthanan <87657214+jana621@users.noreply.github.com>
2022-09-06 01:59:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Grid container spacing={3}>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/de.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/de.png alt="""></img>
             <h3>Germany</h3>
             <p><b>Polupation:</b> 81,770,900</p>
             <p><b>Region:</b> Europe</p>
@@ -17,7 +17,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/us.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/us.png alt="""></img>
             <h3>United States of America</h3>
             <p><b>Polupation:</b> 323,947,000</p>
             <p><b>Region:</b> Americas</p>
@@ -26,7 +26,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/br.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/br.png" alt="" alt=""></img>
             <h3>Brazil</h3>
             <p><b>Polupation:</b> 206,135,803</p>
             <p><b>Region:</b> Americas</p>
@@ -35,7 +35,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/is.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/is.png" alt=""></img>
             <h3>Iceland</h3>
             <p><b>Polupation:</b> 334,300</p>
             <p><b>Region:</b> Europe</p>
@@ -44,7 +44,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/af.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/af.png" alt=""></img>
             <h3>Afghanistan</h3>
             <p><b>Polupation:</b> 27,657,145</p>
             <p><b>Region:</b> Asia</p>
@@ -53,7 +53,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/ax.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/ax.png" alt=""></img>
             <h3>Aland Islands</h3>
             <p><b>Polupation:</b> 28,875</p>
             <p><b>Region:</b> Europe</p>
@@ -62,7 +62,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/al.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/al.png" alt=""></img>
             <h3>Albania</h3>
             <p><b>Polupation:</b> 2,886,026</p>
             <p><b>Region:</b> Europe</p>
@@ -71,7 +71,7 @@ function App() {
         </Grid>
         <Grid item xs={3}>
           <Paper>
-            <img src="https://flagpedia.net/data/flags/w580/dz.png"></img>
+            <img src="https://flagpedia.net/data/flags/w580/dz.png" alt=""></img>
             <h3>Algeria</h3>
             <p><b>Polupation:</b> 40,400,000</p>
             <p><b>Region:</b> Africa</p>
